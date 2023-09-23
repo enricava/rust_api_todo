5af228170565f1c7457b68d17951aa6b814e52f0
enricava <enricava@ucm.es>
2023-09-23 11:42:19 +0200

    Checkpoint

--- src/handlers.rs
+++ src/handlers.rs
@@ -4,13 +4,23 @@ use axum::{
     response::IntoResponse,
     Json,
 };
-use serde_json::error;
 use uuid::Uuid;
 
 use crate::{
     model::{QueryOptions, Todo, UpdateTodoSchema, DB},
     response::{SingleTodoResponse, TodoData, TodoListResponse},
 };
+
+pub async fn health_checker_handler() -> impl IntoResponse {
+    const MESSAGE: &str = "CRUD API Running";
+
+    let json_response = serde_json::json!({
+        "status": "success",
+        "message": MESSAGE
+    });
+
+    Json(json_response)
+}
 
 pub async fn todos_list_handler(opts: Option<Query<QueryOptions>>, State(db): State<DB>) -> impl IntoResponse {
     let todos = db.lock().await;
@@ -84,5 +94,66 @@ pub async fn get_todo_handler(Path(id): Path<Uuid>, State(db): State<DB>) -> Res
         "status": "fail",
         "message": format!("Todo with ID: {} not found", id)
     });
+    Err((StatusCode::NOT_FOUND, Json(error_response)))
+}
+
+pub async fn edit_todo_handler(Path(id): Path<Uuid>, State(db): State<DB>, Json(body): Json<UpdateTodoSchema>) -> Result<impl IntoResponse, (StatusCode, Json<serde_json::Value>)> {
+    let id = id.to_string();
+    let mut todos = db.lock().await;
+
+    if let Some(todo) = todos.iter_mut().find(|todo| todo.id == Some(id.clone())) {
+        let datetime = chrono::Utc::now();
+        let title = body.title.to_owned().unwrap_or_else(|| todo.title.to_owned());
+        let content = body.content.to_owned().unwrap_or_else(|| todo.content.to_owned());
+        let completed = body.completed.unwrap();
+        let payload = Todo {
+            id: todo.id.to_owned(),
+            title: if !title.is_empty() {
+                title
+            } else {
+                todo.title.to_owned()
+            },
+            content: if !content.is_empty() {
+                content
+            } else {
+                todo.content.to_owned()
+            },
+            completed: Some(completed),
+            date_created: todo.date_created,
+            date_updated: Some(datetime)
+        };
+        *todo = payload;
+
+        let json_response = SingleTodoResponse {
+            status: "success".to_string(),
+            data: TodoData { todo: todo.clone() }
+        };
+        Ok((StatusCode::OK, Json(json_response)))
+
+    } else {
+        let error_response = serde_json::json!({
+            "status": "fail",
+            "message": format!("Todo {} not found", id)
+        });
+
+        Err((StatusCode::NOT_FOUND, Json(error_response)))
+    }
+
+}
+
+pub async fn delete_todo_handler(Path(id): Path<Uuid>, State(db): State<DB>) -> Result<impl IntoResponse, (StatusCode, Json<serde_json::Value>)> {
+    let id = id.to_string();
+    let mut todos = db.lock().await;
+
+    if let Some(todo_idx) = todos.iter().position(|todo| todo.id == Some(id.clone())) {
+        todos.remove(todo_idx);
+        return Ok((StatusCode::NO_CONTENT, Json("")));
+    }
+
+    let error_response = serde_json::json!({
+        "status": "fail",
+        "message": format!("Todo {} not found", id)
+    });
+
     Err((StatusCode::NOT_FOUND, Json(error_response)))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,23 @@
-use axum::{response::IntoResponse, routing::get, Json, Router};
 mod handlers;
 mod model;
 mod response;
+mod route;
 
-async fn health_checker_handler() -> impl IntoResponse {
-    const MESSAGE: &str = "Build Simple CRUD API in Rust using Axum";
-
-    let json_response = serde_json::json!({
-        "status": "success",
-        "message": MESSAGE
-    });
-
-    Json(json_response)
-}
+use axum::http::{
+    header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE},
+    HeaderValue, Method,
+};
+use route::create_router;
+use tower_http::cors::CorsLayer;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/api/healthchecker", get(health_checker_handler));
+    let cors = CorsLayer::new()
+        .allow_methods([Method::GET, Method::POST, Method::PATCH, Method::DELETE])
+        .allow_credentials(true)
+        .allow_headers([AUTHORIZATION, ACCEPT, CONTENT_TYPE]);
+
+    let app = create_router().layer(cors);
 
     println!("🚀 Server started successfully");
     axum::Server::bind(&"0.0.0.0:8000".parse().unwrap())
